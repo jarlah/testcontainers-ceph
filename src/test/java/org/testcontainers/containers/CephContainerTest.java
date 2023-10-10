537547765959593af069bf9633a9d6f575804045
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-10 08:35:49 +0200

    revert container test changes

--- src/test/java/org/testcontainers/containers/CephContainerTest.java
+++ src/test/java/org/testcontainers/containers/CephContainerTest.java
@@ -1,31 +1,17 @@
 package org.testcontainers.containers;
 
-import com.amazonaws.SDKGlobalConfiguration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.amazonaws.services.s3.model.SSECustomerKey;
 import org.junit.Test;
 import org.testcontainers.utility.DockerImageName;
 
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,68 +22,45 @@ public class CephContainerTest {
     public void testBasicUsage() throws Exception {
         try (
             // minioContainer {
-            CephContainer container = new CephContainer()
+            CephContainer container = new CephContainer();
             // }
         ) {
             container.start();
-            // We should not modify demo script by default
-            assertThat(getDemoScriptFromContainer(container)).doesNotContain(
-                    "rgw frontends = ${RGW_FRONTEND}\n" +
-                    "rgw verify ssl = false\n" +
-                    "rgw crypt require ssl = false"
-            );
+
             assertThat(container.getCephAccessKey()).isEqualTo("demo");
             assertThat(container.getCephSecretKey()).isEqualTo("demo");
             assertThat(container.getCephBucket()).isEqualTo("demo");
 
             AmazonS3 s3client = getS3client(container);
 
-            s3client.createBucket("test-bucket");
-            assertThat(s3client.doesBucketExistV2("test-bucket")).isTrue();
+            assertThat(s3client.doesBucketExistV2(container.getCephBucket())).isTrue();
 
             URL file = this.getClass().getResource("/object_to_upload.txt");
             assertThat(file).isNotNull();
-            s3client.putObject("test-bucket", "my-objectname", file.getFile());
+            s3client.putObject(container.getCephBucket(), "my-objectname", file.getFile());
 
-            List<S3ObjectSummary> objets = s3client.listObjectsV2("test-bucket").getObjectSummaries();
+            List<S3ObjectSummary> objets = s3client.listObjectsV2(container.getCephBucket()).getObjectSummaries();
             assertThat(objets.size()).isEqualTo(1);
             assertThat(objets.get(0).getKey()).isEqualTo("my-objectname");
         }
     }
 
     @Test
-    public void testOverrides() throws Exception {
-        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
+    public void testOverrides() throws URISyntaxException {
         try (
             // cephOverrides {
             CephContainer container = new CephContainer("quay.io/ceph/demo:latest")
                 .withCephAccessKey("testuser123")
                 .withCephSecretKey("testpassword123")
                 .withCephBucket("testbucket123")
-                .withSslDisabled()
             // }
         ) {
             container.start();
-            // we should have modified the demo script
-            assertThat(getDemoScriptFromContainer(container)).contains(
-                    "rgw frontends = ${RGW_FRONTEND}\n" +
-                    "rgw verify ssl = false\n" +
-                    "rgw crypt require ssl = false"
-            );
             assertThat(container.getCephAccessKey()).isEqualTo("testuser123");
             assertThat(container.getCephSecretKey()).isEqualTo("testpassword123");
             assertThat(container.getCephBucket()).isEqualTo("testbucket123");
             AmazonS3 s3client = getS3client(container);
-            s3client.createBucket("lala");
-            assertThat(s3client.doesBucketExistV2("lala")).isTrue();
-
-            URL file = this.getClass().getResource("/object_to_upload.txt");
-            assertThat(file).isNotNull();
-
-            Key key = getPasswordBasedKey("AES", 256, "password".toCharArray());
-            PutObjectRequest request = new PutObjectRequest("lala", "my-objectname", file.getFile());
-            request.setSSECustomerKey(new SSECustomerKey(key.getEncoded()));
-            s3client.putObject(request);
+            assertThat(s3client.doesBucketExistV2("testbucket123")).isTrue();
         }
     }
 
@@ -125,7 +88,6 @@ public class CephContainerTest {
     }
 
     // configuringClient {
-
     private static AmazonS3 getS3client(CephContainer container) throws URISyntaxException {
         AWSCredentials credentials = new BasicAWSCredentials(
                 container.getCephAccessKey(),
@@ -143,18 +105,4 @@ public class CephContainerTest {
                 .build();
     }
     // }
-
-    private static String getDemoScriptFromContainer(CephContainer container) throws IOException {
-        container.copyFileFromContainer("/opt/ceph-container/bin/demo", "demo-script");
-        return new String(Files.readAllBytes(Paths.get("demo-script")));
-    }
-
-    private static Key getPasswordBasedKey(String cipher, int keySize, char[] password) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        byte[] salt = new byte[100];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(salt);
-        PBEKeySpec pbeKeySpec = new PBEKeySpec(password, salt, 1000, keySize);
-        SecretKey pbeKey = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256").generateSecret(pbeKeySpec);
-        return new SecretKeySpec(pbeKey.getEncoded(), cipher);
-    }
 }
