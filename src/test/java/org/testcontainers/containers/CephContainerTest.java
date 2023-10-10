264b852535a0837d22c606e8c088f108c7d50c19
Jarl André Hübenthal <jarlah@protonmail.com>
2023-10-10 08:42:11 +0200

    refactor test (remove sse-c testing from main tests)

--- src/test/java/org/testcontainers/containers/CephContainerTest.java
+++ src/test/java/org/testcontainers/containers/CephContainerTest.java
@@ -10,8 +10,11 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.junit.Test;
 import org.testcontainers.utility.DockerImageName;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,45 +25,59 @@ public class CephContainerTest {
     public void testBasicUsage() throws Exception {
         try (
             // minioContainer {
-            CephContainer container = new CephContainer();
+            CephContainer container = new CephContainer()
             // }
         ) {
             container.start();
-
+            // We should not modify demo script by default
+            assertThat(getDemoScriptFromContainer(container)).doesNotContain(
+                    "rgw frontends = ${RGW_FRONTEND}\n" +
+                    "rgw verify ssl = false\n" +
+                    "rgw crypt require ssl = false"
+            );
             assertThat(container.getCephAccessKey()).isEqualTo("demo");
             assertThat(container.getCephSecretKey()).isEqualTo("demo");
             assertThat(container.getCephBucket()).isEqualTo("demo");
 
             AmazonS3 s3client = getS3client(container);
 
-            assertThat(s3client.doesBucketExistV2(container.getCephBucket())).isTrue();
+            assertThat(s3client.doesBucketExistV2("test-bucket")).isTrue();
 
             URL file = this.getClass().getResource("/object_to_upload.txt");
             assertThat(file).isNotNull();
-            s3client.putObject(container.getCephBucket(), "my-objectname", file.getFile());
+            s3client.putObject("test-bucket", "my-objectname", file.getFile());
 
-            List<S3ObjectSummary> objets = s3client.listObjectsV2(container.getCephBucket()).getObjectSummaries();
+            List<S3ObjectSummary> objets = s3client.listObjectsV2("test-bucket").getObjectSummaries();
             assertThat(objets.size()).isEqualTo(1);
             assertThat(objets.get(0).getKey()).isEqualTo("my-objectname");
         }
     }
 
     @Test
-    public void testOverrides() throws URISyntaxException {
+    public void testOverrides() throws Exception {
         try (
             // cephOverrides {
             CephContainer container = new CephContainer("quay.io/ceph/demo:latest")
                 .withCephAccessKey("testuser123")
                 .withCephSecretKey("testpassword123")
                 .withCephBucket("testbucket123")
+                .withSslDisabled()
             // }
         ) {
             container.start();
+            // we should have modified the demo script
+            assertThat(getDemoScriptFromContainer(container)).contains(
+                    "rgw frontends = ${RGW_FRONTEND}\n" +
+                    "rgw verify ssl = false\n" +
+                    "rgw crypt require ssl = false"
+            );
             assertThat(container.getCephAccessKey()).isEqualTo("testuser123");
             assertThat(container.getCephSecretKey()).isEqualTo("testpassword123");
             assertThat(container.getCephBucket()).isEqualTo("testbucket123");
             AmazonS3 s3client = getS3client(container);
             assertThat(s3client.doesBucketExistV2("testbucket123")).isTrue();
+            s3client.createBucket("some-other-bucket");
+            assertThat(s3client.doesBucketExistV2("some-other-bucket")).isTrue();
         }
     }
 
@@ -88,6 +105,7 @@ public class CephContainerTest {
     }
 
     // configuringClient {
+
     private static AmazonS3 getS3client(CephContainer container) throws URISyntaxException {
         AWSCredentials credentials = new BasicAWSCredentials(
                 container.getCephAccessKey(),
@@ -105,4 +123,9 @@ public class CephContainerTest {
                 .build();
     }
     // }
+
+    private static String getDemoScriptFromContainer(CephContainer container) throws IOException {
+        container.copyFileFromContainer("/opt/ceph-container/bin/demo", "demo-script");
+        return new String(Files.readAllBytes(Paths.get("demo-script")));
+    }
 }
