d38f2cb7365a86fa9a37981ed4de59da6e119938
vandalist <feddevanderlist@gmail.com>
2024-11-28 09:07:28 +0100

    fix linter issues to be more in line with java rules

--- src/test/java/org/testcontainers/containers/CephContainerTest.java
+++ src/test/java/org/testcontainers/containers/CephContainerTest.java
@@ -38,21 +38,21 @@ import java.util.Objects;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class CephContainerTest {
+class CephContainerTest {
 
     @Test
-    public void testBasicUsage() throws Exception {
+    void testBasicUsage() throws Exception {
         try (
-            // minioContainer {
-            CephContainer container = new CephContainer()
-            // }
+                // minioContainer {
+                CephContainer container = new CephContainer()
+                // }
         ) {
             container.start();
             // We should not modify demo script by default
             assertThat(getDemoScriptFromContainer(container)).doesNotContain(
                     "rgw frontends = ${RGW_FRONTEND}\n" +
-                    "rgw verify ssl = false\n" +
-                    "rgw crypt require ssl = false"
+                            "rgw verify ssl = false\n" +
+                            "rgw crypt require ssl = false"
             );
             assertThat(container.getCephAccessKey()).isEqualTo("demo");
             assertThat(container.getCephSecretKey()).isSameAs("b36361c4-1589-42f7-a369-d9dafb926d55");
@@ -63,9 +63,9 @@ public class CephContainerTest {
 
             HeadBucketResponse headBucketResponse = s3client.headBucket(HeadBucketRequest.builder().bucket("demo").build());
             assertThat(headBucketResponse.sdkHttpResponse().isSuccessful())
-                .isTrue();
+                    .isTrue();
             assertThat(headBucketResponse.sdkHttpResponse().headers().get("X-RGW-Quota-Max-Buckets").get(0))
-                .isEqualTo("1000");
+                    .isEqualTo("1000");
 
             final String key = "my-objectname";
 
@@ -76,41 +76,41 @@ public class CephContainerTest {
             assertThat(putObjectResponse.expiration()).isNull();
 
             ResponseInputStream<GetObjectResponse>
-                responseInputStream = s3client.getObject(request -> request.bucket("demo").key(key));
+                    responseInputStream = s3client.getObject(request -> request.bucket("demo").key(key));
             assertThat(responseInputStream).hasContent("This is a file");
 
-            ListObjectsV2Response objets = s3client.listObjectsV2((builder) -> builder.bucket("demo"));
-            assertThat(objets.contents().size()).isEqualTo(1);
+            ListObjectsV2Response objets = s3client.listObjectsV2(builder -> builder.bucket("demo"));
+            assertThat(objets.contents()).hasSize(1);
             assertThat(objets.contents().get(0).key()).isEqualTo(key);
 
-            assertThatThrownBy(() -> {
-                s3client.putObject(
-                        getSSECPutObjectRequest("demo", key),
-                        RequestBody.fromFile(getTestFile())
-                );
-            })
+            assertThatThrownBy(() ->
+                    s3client.putObject(
+                            getSSECPutObjectRequest("demo", key),
+                            RequestBody.fromFile(getTestFile())
+                    )
+            )
                     .isInstanceOf(software.amazon.awssdk.services.s3.model.S3Exception.class)
                     .hasMessageContaining("Service: S3, Status Code: 400, Request ID: ");
         }
     }
 
     @Test
-    public void testOverrides() throws Exception {
+    void testOverrides() throws Exception {
         try (
-            // cephOverrides {
-            CephContainer container = new CephContainer("quay.io/ceph/demo:latest")
-                .withCephAccessKey("testuser123")
-                .withCephSecretKey("testpassword123")
-                .withCephBucket("testbucket123")
-                .withSslDisabled()
-            // }
+                // cephOverrides {
+                CephContainer container = new CephContainer("quay.io/ceph/demo:latest")
+                        .withCephAccessKey("testuser123")
+                        .withCephSecretKey("testpassword123")
+                        .withCephBucket("testbucket123")
+                        .withSslDisabled()
+                // }
         ) {
             container.start();
             // we should have modified the demo script
             assertThat(getDemoScriptFromContainer(container)).contains(
                     "rgw frontends = ${RGW_FRONTEND}\n" +
-                    "rgw verify ssl = false\n" +
-                    "rgw crypt require ssl = false"
+                            "rgw verify ssl = false\n" +
+                            "rgw crypt require ssl = false"
             );
             assertThat(container.getWaitStrategy()).isInstanceOf(LogMessageWaitStrategy.class);
             assertThat(container.getCephAccessKey()).isEqualTo("testuser123");
@@ -132,7 +132,7 @@ public class CephContainerTest {
     }
 
     @Test
-    public void testSpecificDaemonImage() throws URISyntaxException {
+    void testSpecificDaemonImage() throws URISyntaxException {
         DockerImageName daemonImage =
                 DockerImageName.parse("quay.io/ceph/daemon:v7.0.3-stable-7.0-quincy-centos-stream8-x86_64")
                         .asCompatibleSubstituteFor("quay.io/ceph/demo");
@@ -165,7 +165,7 @@ public class CephContainerTest {
                         .withCephAccessKey("testuser123")
                         .withCephSecretKey("testpassword123")
                         .withCephBucket("testbucket123")
-                        .withCommand("demo");
+                        .withCommand("demo")
         ) {
             container.setWaitStrategy(Wait.forListeningPort());
             container.start();
@@ -184,7 +184,7 @@ public class CephContainerTest {
                         .withCephAccessKey("testuser123")
                         .withCephSecretKey("testpassword123")
                         .withCephBucket("testbucket123")
-                        .withCommand("demo");
+                        .withCommand("demo")
         ) {
             container.waitingFor(Wait.forListeningPort());
             container.start();
@@ -215,6 +215,7 @@ public class CephContainerTest {
         container.copyFileFromContainer("/opt/ceph-container/bin/demo", "demo-script");
         return new String(Files.readAllBytes(Paths.get("demo-script")));
     }
+
     @NotNull
     private File getTestFile() throws URISyntaxException {
         return Paths.get(Objects.requireNonNull(this.getClass().getResource("/object_to_upload.txt")).toURI()).toFile();
@@ -228,7 +229,7 @@ public class CephContainerTest {
         return SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256").generateSecret(pbeKeySpec);
     }
 
-    private static PutObjectRequest getSSECPutObjectRequest(String bucket, String key) throws NoSuchAlgorithmException, InvalidKeySpecException, URISyntaxException {
+    private static PutObjectRequest getSSECPutObjectRequest(String bucket, String key) throws NoSuchAlgorithmException, InvalidKeySpecException {
         SecretKey secretKey = getPasswordBasedKey("testtesttesttesttesttest123".toCharArray());
         byte[] customerKeyBytes = secretKey.getEncoded();
         String customerKeyMD5;
